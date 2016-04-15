d35d59a17a752334c7982cf77ac64a1ad78090a2
Megan Walter <megwalter@hotmail.com>
2016-04-14 23:20:16 -0400

    made some changes to server js and forecast js for testing

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ var Forecast = require('forecast.io');
 var options = {
   APIKey: process.env.FORECAST_API_KEY
 },
+forecast = new Forecast(options);
+
+// var time = new Date().setDate(0);
+// forecast.getAtTime(latitude, longitude, time, function(err, res, data) {
+//   if (err) throw err;
+//   log('res: ' + util.inspect(res));
+//   log('data: ' + util.inspect(data));
+// });
 
 var requestProxy = require('express-request-proxy'),
   express = require('express'),
