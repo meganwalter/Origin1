54f3202b8ef71fc6b25527d7fa3c42931fb4ad98
Megan Walter <megwalter@hotmail.com>
2016-04-17 18:01:01 -0400

    worked on getting elephant sql working and using that api

--- server.js
+++ server.js
@@ -1,39 +1,59 @@
-var Forecast = require('forecast.io');
-
-var options = {
-  APIKey: process.env.FORECAST_API_KEY
-},
-forecast = new Forecast(options);
-
-// var time = new Date().setDate(0);
-// forecast.getAtTime(latitude, longitude, time, function(err, res, data) {
-//   if (err) throw err;
-//   log('res: ' + util.inspect(res));
-//   log('data: ' + util.inspect(data));
-// });
-
 var requestProxy = require('express-request-proxy'),
   express = require('express'),
   port = process.env.PORT || 3000,
   app = express();
 
-var proxyGitHub = function(request, response) {
-  console.log('Routing GitHub request for', request.params[0]);
-  (requestProxy({
-    url: 'https://api.github.com/' + request.params[0],
-    headers: { Authorization: 'token ' + process.env.GITHUB_TOKEN }
-  }))(request, response);
-};
-
-app.get('/github/*', proxyGitHub);
+var pg = require('pg');
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('*', function(request, response) {
+app.get('/', function(request, response) {
   console.log('New request:', request.url);
   response.sendFile(__dirname + '/public/index.html', { root: '.' });
+});
+
+
+//this is where postgres starts
+//or native libpq bindings
+var conString = process.env.ELEPHANTSQL_URL || "postgres://ezzwtbet:MzS6fXklZnlb-n2qtS8W-4Y2QmBmzzIK@pellefant.db.elephantsql.com:5432/ezzwtbet";
+//var pg = require('pg').native
+app.get('/api/origin1', function(req, res) {
+  var results = [];
+  pg.connect(conString, function(err, client, done){
+    if (err) {
+      done();
+      console.log(err);
+      return res.status(500).json({ success: false, data: err });
+    }
+
+    var query = client.query('SELECT * FROM origin1 ORDER BY year, month ASC;');
+
+    query.on('row', function(row) {
+      results.push(row);
+    });
+
+    query.on('end', function(){
+      done();
+      return res.json(results);
+    });
+  });
 });
 
 app.listen(port, function() {
   console.log('Server started on port ' + port + '!');
 });
+
+// var client = new pg.Client(conString);
+// client.connect(function(err) {
+//   if (err) {
+//     return console.error('could not connect to postgres', err);
+//   }
+//   client.query('SELECT * from origin1', function(err, result) {
+//     if (err) {
+//       return console.error('error running query', err);
+//     }
+//     console.log(result);
+//     //output should be database
+//     client.end();
+//   });
+// });
