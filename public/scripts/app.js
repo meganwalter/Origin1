625cbb24026ca015ae04675918b5f54eafc74911
Stephanie Lopez <lopezlstephanie@gmail.com>
2016-04-18 17:09:00 -0700

    added event handler to form button to store info in an object

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,23 @@
 // TODO: add controller for data storage of user contact info and deploy to firebbase
+var formData = [];
+
+function clearForm(event) {
+  $('form').find(':checkbox').removeAttr('checked').end()
+  .find('input, select, textarea').val('');
+  return false;
+}
+
+$('#submitButton').on('click',function(event){
+  event.preventDefault();
+  var formInput = {};
+  $('form').find('input, select, textarea').each(function() {
+    var field = $(this).attr('id');
+    var value = $(this).val();
+    formInput[field] = value;
+  });
+  formData.push(formInput);
+  clearForm();
+});
 
 
 
