ec53a4e0d1480396ea26d59795b52ea95d9bd73b
Andrew Watson <andrewpeterwatson@gmail.com>
2016-04-19 21:15:35 -0700

    work on coffee drop down

--- public/scripts/coffeePageView.js
+++ public/scripts/coffeePageView.js
@@ -15,7 +15,11 @@ Coffee.prototype.toHtml = function() {
   var template = Handlebars.compile(templateSrc);
   return template(this);
 };
+var guat = "guatemalaPage.json";
+var hon = "hondurasPage.json";
+var nic = "nicaraguaPage.json";
 
+var selectCountry = [guat, hon, nic];
 Coffee.all = [];
 
 Coffee.loadAll = function(rawData) {
@@ -23,13 +27,19 @@ Coffee.loadAll = function(rawData) {
       return new Coffee(pd)
   });
 }
+
+Coffee.menuOption = function() {
+  var id = $('.menuOptions');
+  var idValue = id.options[id.selectedIndex].value;
+  console.log(idValue);
+}
 Coffee.fetchAll = function(callBack) {
   if (localStorage.rawData) {
     Coffee.loadAll(JSON.parse(localStorage.rawData));
     callBack();
   } else {
     $.ajax ({
-      url: "../data/nicaraguaPage.json",
+      url: "../data/" + selectCountry[0],
       method: "GET"
     }) .done(function(data, message, xhr) {
         localStorage.setItem('rawData', JSON.stringify(data));
