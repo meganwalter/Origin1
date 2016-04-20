1417db570943ff66850d1b70d5d1d250dabf7238
Megan Walter <megwalter@hotmail.com>
2016-04-19 23:05:09 -0400

    added more objects and filters for us to be able to work with

--- public/scripts/guatemala.js
+++ public/scripts/guatemala.js
@@ -1,39 +1,66 @@
 (function(module) {
 
-var guatemala = {};
+  var origin = {};
+  origin.all = [];
+  var guatemala = {};
+  guatemala.all = [];
+  var nicaragua = {};
+  nicaragua.all = [];
+  var honduras = {};
+  honduras.all = [];
+  var gsguatemala = {};
+  gsguatemala.all = [];
+  var gsnicaragua = {};
+  gsnicaragua.all = [];
+  var gshonduras = {};
+  gshonduras.all = [];
 
-guatemala.all = [];
+  origin.getData = function() {
 
-var origin = {};
+    $.ajax(
+      {
+        url: '/api/origin1/',
+        type: 'GET',
+        success: function(data) {
+          origin.all = data;
+          console.log(origin.all);
+        },
 
-origin.all = [];
-
-origin.getData = function() {
-
-  $.ajax(
-    {
-      url: '/api/origin1/',
-      type: 'GET',
-      success: function(data) {
-        origin.all = data;
-        console.log(origin.all);
-      },
-
-      error: function() {
-        console.error();
-      },
-    })
+        error: function() {
+          console.error();
+        },
+      })
     .done(function(n) {
       guatemala.all = origin.all.filter(function(jsondata) {
         return jsondata.country === 'guatemala';
       });
+      nicaragua.all = origin.all.filter(function(jsondata) {
+        return jsondata.country === 'nicaragua';
+      });
+      honduras.all = origin.all.filter(function(jsondata) {
+        return jsondata.country === 'honduras';
+      });
+      gsguatemala.all = origin.all.filter(function(jsondata) {
+        return jsondata.country === 'gsguatemala';
+      });
+      gsnicaragua.all = origin.all.filter(function(jsondata) {
+        return jsondata.country === 'gsnicaragua';
+      });
+      gshonduras.all = origin.all.filter(function(jsondata) {
+        return jsondata.country === 'gshonduras';
+      });
     }
   );
-};
+  };
 
-origin.getData();
+  origin.getData();
 
-module.origin = origin;
-module.guatemala = guatemala;
+  module.origin = origin;
+  module.guatemala = guatemala;
+  module.nicaragua = nicaragua;
+  module.honduras = honduras;
+  module.gsguatemala = gsguatemala;
+  module.gsnicaragua = gsnicaragua;
+  module.gshonduras = gshonduras;
 
 })(window);
